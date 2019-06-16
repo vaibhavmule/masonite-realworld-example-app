90c60ac5d48adc9b3cc436826343eb1ad8a02508
Joseph Mancuso <idmann509@gmail.com>
2019-06-15 22:11:37 -0400

    Added tests

--- tests/unit/test_article.py
+++ tests/unit/test_article.py
@@ -31,8 +31,16 @@ class TestArticle(TestCase):
             'token': None
         })
 
+        Article.create({
+            'title': 'this is a title',
+            'slug': 'this-is-a-title',
+            'description': 'This is a description',
+            'body': 'this is a body',
+            'author_id': 1
+        })
+
     def test_can_create_article(self):
-        self.assertTrue(self.get('/api/articles').hasJson('articlesCount', 0))
+        self.assertTrue(self.get('/api/articles').hasJson('articlesCount', 1))
 
         self.actingAs(User.find(1)).json('POST', '/api/articles', {
                 "article": {
@@ -55,3 +63,56 @@ class TestArticle(TestCase):
         })
 
         self.assertTrue(Article.all().count())
+
+    def test_article_can_update(self):
+        article = Article.find(1)
+        self.assertEqual(article.slug, 'this-is-a-title')
+        self.assertEqual(article.tags.count(), 0)
+
+        self.json('PUT', '/api/articles/this-is-a-title', {
+            "article": {
+                "slug": "this-is-another-slug",
+                "title": "How to train your dragon",
+                "description": "Dragons",
+                "body": "I Love Dragons",
+                "tagList": ['kids', 'adults']
+            }
+        })
+
+        article = Article.find(1)
+        self.assertEqual(article.slug, 'this-is-another-slug')
+        self.assertEqual(article.tags.count(), 2)
+    
+    def test_article_can_be_deleted(self):
+        Article.create({
+            'title': 'delete this article',
+            'slug': 'delete-this-article',
+            'description': 'This is a description',
+            'body': 'this is a body',
+            'author_id': 1
+        })
+
+        article = Article.where('slug', 'delete-this-article').first()
+        self.assertEqual(article.slug, 'delete-this-article')
+
+        self.json('DELETE', '/api/articles/delete-this-article')
+
+        article = Article.where('slug', 'delete-this-article').first()
+        self.assertFalse(article)
+
+    def test_can_index(self):
+        self.assertTrue(
+            self.json('GET', '/api/articles').hasAmount('articles', 2)
+        )
+
+    def test_can_show_single_json(self):
+        Article.create({
+            'title': 'this article',
+            'slug': 'this-article',
+            'description': 'This is a description',
+            'body': 'this is a body',
+            'author_id': 1
+        })
+        self.assertTrue(
+            self.json('GET', '/api/articles/this-article').hasJson('article.slug', 'this-article')
+        )
